88ef84977c7bdaede170f9173818fb9dd64e945c
LucioSandoval <lucio.scaceres@gmail.com>
2023-11-01 19:25:13 -0300

    Encuesta dojo

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using EncuestaDojo.Models;
+
+namespace EncuestaDojo.Controllers;
+
+public class HomeController : Controller
+{
+    
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+    
+    //[HttpPost]
+    [Route("")]
+    public IActionResult Index(string nombre, string apellido, string ubicacion, string lenguaje, string descripcion)
+    {
+    return View();
+    }
+
+
+    [HttpPost] 
+    [Route("/resul")]
+    public IActionResult ProcessForm(string nombre, string apellido, string ubicacion, string lenguaje, string descripcion)
+    {
+        List<string> lista = new List<string>
+        {
+            nombre,
+            apellido,
+            ubicacion,
+            lenguaje,
+            descripcion
+        };
+
+        return View("Privacy", lista);
+    }
+
+
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
